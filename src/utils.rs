5d68ba34b86bf04e8747e61a60aae5ad77b2ff6a
Marco Grassi <marco.gra@gmail.com>
2022-06-27 18:02:04 +0800

    added tests

--- src/utils.rs
+++ src/utils.rs
@@ -9,3 +9,27 @@ pub fn url_is_valid(url: &str) -> bool {
     }
     false
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    pub fn ok_urls() {
+        let ok = [
+            "http://www.test.com",
+            "https://sub.test.com",
+            "ftp://ftp.test.com",
+        ];
+        let not_ok = [
+            "file://repo.test.com",
+            "asdasd://",
+            "js:",
+            "smb://storage.test.com",
+        ];
+        for s in ok {
+            assert!(super::url_is_valid(s), "{} should be a valid url", s);
+        }
+        for s in not_ok {
+            assert!(!super::url_is_valid(s), "{} should be a invalid url", s)
+        }
+    }
+}
