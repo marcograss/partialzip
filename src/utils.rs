30fd0f2825c18daef937856479b118c02ce10bb0
Marco Grassi <marco.gra@gmail.com>
2022-09-14 14:35:49 +0800

    code quality improvements

--- src/utils.rs
+++ src/utils.rs
@@ -3,8 +3,8 @@ use url::Url;
 /// Returns if a URL is a valid URL string supported by the library
 #[must_use]
 pub fn url_is_valid(url: &str) -> bool {
-    match Url::parse(url) {
-        Ok(url) => ["http", "https", "ftp", "file"].contains(&url.scheme()),
-        Err(_) => false,
-    }
+    Url::parse(url).map_or(false, |url| {
+        // Supported URL schemes
+        ["http", "https", "ftp", "file"].contains(&url.scheme())
+    })
 }
