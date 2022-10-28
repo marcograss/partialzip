f14738240112fc773b9ca5e614e07daecc4f2845
Marco Grassi <marco.gra@gmail.com>
2022-10-29 01:25:18 +0800

    add benchmarks

---
+++ benches/pz_benchmark.rs
@@ -0,0 +1,22 @@
+#![cfg(unix)]
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    use std::path::PathBuf;
+
+    use partialzip::partzip::PartialZip;
+
+    c.bench_function("local file benchmark", |b| {
+        b.iter(|| {
+            let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            d.push("testdata/test.zip");
+            let mut pz = PartialZip::new(&format!("file://localhost{}", d.display()))
+                .expect("cannot create PartialZip in benchmark");
+            let _ = pz.list();
+            let _ = pz.download("1.txt");
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
