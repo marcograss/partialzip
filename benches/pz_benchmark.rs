ca34f6a203091f1d6c534a1c4098db518a314414
Marco Grassi <marco.gra@gmail.com>
2023-10-05 09:19:44 +0800

    more detailed benchmarks

--- benches/pz_benchmark.rs
+++ benches/pz_benchmark.rs
@@ -6,13 +6,32 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     use partialzip::partzip::PartialZip;
 
-    c.bench_function("local file benchmark", |b| {
+    c.bench_function("local file benchmark detailed list", |b| {
+        b.iter(|| {
+            let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            d.push("testdata/test.zip");
+            let mut pz = PartialZip::new(&format!("file://localhost{}", d.display()))
+                .expect("cannot create PartialZip in benchmark detailed");
+            let _list = pz.list_detailed();
+        });
+    });
+
+    c.bench_function("local file benchmark list names", |b| {
         b.iter(|| {
             let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
             d.push("testdata/test.zip");
             let mut pz = PartialZip::new(&format!("file://localhost{}", d.display()))
                 .expect("cannot create PartialZip in benchmark");
-            let _list = pz.list_detailed();
+            let _list = pz.list_names();
+        });
+    });
+
+    c.bench_function("local file download", |b| {
+        b.iter(|| {
+            let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            d.push("testdata/test.zip");
+            let mut pz = PartialZip::new(&format!("file://localhost{}", d.display()))
+                .expect("cannot create PartialZip in benchmark download");
             let _download = pz.download("1.txt");
         });
     });
