54858bd340637ee0d31545c5683a176fed04c2f4
Marco Grassi <marco.gra@gmail.com>
2025-01-25 14:34:48 +0800

    fix clippy

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use url::Url;
 /// Returns if a URL is a valid URL string supported by the library
 #[must_use]
 pub fn url_is_valid(url: &str) -> bool {
-    Url::parse(url).map_or(false, |url| {
+    Url::parse(url).is_ok_and(|url| {
         // Supported URL schemes
         ["http", "https", "ftp", "file"].contains(&url.scheme())
     })
